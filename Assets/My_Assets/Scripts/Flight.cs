8372d351957df6e2e999878cc341a861bb797478
GONBEEEproject-X <gonbeee@xvi.co.jp>
2020-06-07 03:05:06 +0900

    ルームモデルやら飛行コードやら

---
+++ Assets/My_Assets/Scripts/Flight.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flight : MonoBehaviour
+{
+    [SerializeField]
+    Transform leftHand, rightHand, head;
+    Vector3 lookForward;
+
+    [SerializeField]
+    float flyThreshold;
+
+    [SerializeField]
+    AnimationCurve accelCurve;
+    float accelTime;
+    bool isAccel;
+
+    float speed;
+    float handDistance;
+
+    private void Update()
+    {
+        SpeedCheck();
+        ForwardCheck();
+
+        if (handDistance >= flyThreshold)
+        {
+            accelTime += Time.deltaTime;
+            if (accelTime >= accelCurve[accelCurve.length - 1].time)
+            {
+                accelTime = accelCurve[accelCurve.length - 1].time;
+            }
+        }
+        else
+        {
+            accelTime -= Time.deltaTime;
+            if (accelTime <= 0)
+            {
+                accelTime = 0;
+            }
+        }
+
+        speed = accelCurve.Evaluate(accelTime);
+    }
+
+    private void SpeedCheck()
+    {
+        handDistance = Vector3.Distance(leftHand.position, rightHand.position);
+    }
+
+    private void ForwardCheck()
+    {
+        lookForward = head.forward;
+    }
+
+}
